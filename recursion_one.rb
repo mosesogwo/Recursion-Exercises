d8a9b8381e73692be641c184687a2d70b85ee3a2
Moses Ogwo <mosesogwo@gmail.com>
2020-04-30 14:55:31 +0100

    add power problem description

--- recursion_one.rb
+++ recursion_one.rb
@@ -1,5 +1,12 @@
-def power
+# PROBLEM: Write a function called power which accepts a base and an exponent. The function should return the power of the base to the exponent. Do not worry about negative bases and exponents.
+
+def power(base, exponent)
+  
 end
+
+#power(2, 0) => 1
+#power(2, 2) => 4
+#power(2, 4) => 16
 
 def factorial
 end
