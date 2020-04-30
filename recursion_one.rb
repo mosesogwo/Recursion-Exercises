40ba1d6c0e3af5ae86a6a770c532fd5caadedb1d
Moses Ogwo <mosesogwo@gmail.com>
2020-04-30 15:43:37 +0100

    add factorial method

--- recursion_one.rb
+++ recursion_one.rb
@@ -5,13 +5,21 @@ def power(base, exp)
   return base * power(base, exp-1)
 end
 
-p power(2, 0) #=> 1
-p power(2, 2) #=> 4
-p power(2, 4) #=> 16
+# p power(2, 0) #=> 1
+# p power(2, 2) #=> 4
+# p power(2, 4) #=> 16
 
 def factorial(n)
-  return 1 if n = 
+  return 0 if n < 0
+  return 1 if n == 0
+  return n * factorial(n - 1)
 end
+
+p factorial(1) #1
+p factorial(2) #4
+p factorial(4) #24
+p factorial(7) #5040
+
 
 def product_of_array
 end
