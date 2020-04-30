a632db701130d08da46e5fab9f8e58ff98409c6f
Moses Ogwo <mosesogwo@gmail.com>
2020-04-30 14:45:13 +0100

    initialize all methods to be developed - def method_name end

---
+++ recursion_one.rb
@@ -0,0 +1,14 @@
+def power
+end
+
+def factorial
+end
+
+def product_of_array
+end
+
+def recursive_range
+end
+
+def fib
+end
---
+++ recursion_two.rb
@@ -0,0 +1,26 @@
+def reverse
+end
+
+def is_palindrome
+end
+
+def some_recursive
+end
+
+def flatten
+end
+
+def capitalize_first
+end
+
+def nested_even_sum
+end
+
+def capitalize_words
+end
+
+def stringify_numbers
+end
+
+def collect_strings
+end
