084489dadf0eb80155776e4c27da198d565d8d84
Moses Ogwo <mosesogwo@gmail.com>
2020-05-04 21:53:41 +0100

    add is_palindrome

--- recursion_two.rb
+++ recursion_two.rb
@@ -7,11 +7,24 @@ def reverse(string, i=0)
   reverse(string, i + 1)
 end
 
-p reverse('awesome')      #'emosewa'
-p reverse('rithmschool')  # 'loohcsmhtir'
+# p reverse('awesome')      #'emosewa'
+# p reverse('rithmschool')  # 'loohcsmhtir'
 
-def is_palindrome
+# A recursive method that returns true if the string is a palindrome, otherwise return false
+def is_palindrome(string)
+  return true if string.size == 1
+  return string[0] == string[1] if string.size == 2
+  return is_palindrome(string[1...-1]) if string[0] == string[-1]
+  return false
+  # return true if string == reverse(string.dup)
+  # return false
 end
+
+p is_palindrome('awesome')  #// false
+p is_palindrome('foobar')   #// false
+p is_palindrome('tacocat')  #// true
+p is_palindrome('amanaplanacanalpanama')      #// true
+p is_palindrome('amanaplanacanalpandemonium') #// false
 
 def some_recursive
 end
